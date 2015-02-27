d64c6ade13aaf57e4b48958c2b8825e211657893
James Larisch <root@jameslarisch.com>
2015-02-26 22:45:06 -0800

    gah programming is fun i forgot

--- art/g.hs
+++ art/g.hs
@@ -2,22 +2,29 @@
 import Graphics.Gloss
 import Graphics.Gloss.Interface.Pure.Game
 
-PlayerHeight = 10
-PlayerWidth = 10
+playerHeight = 100
+playerWidth = 10
+ballRadius = 9.0
 
-data Player = Player { x :: Float, y :: Float } 
+data Player = Player { playerx :: Float, playery :: Float } 
+data Ball = Ball { ballx :: Float, bally :: Float } 
+data World = World { player1 :: Player,
+                     player2 :: Player,
+                     ball :: Ball }
 
-data World = World { player1 :: Picture,
-                     player2 :: Picture,
-                     ball :: Picture
-                   }
+drawPlayer :: Player -> Picture
+drawPlayer player = Translate (playerx player) (playery player) (rectangleSolid playerWidth playerHeight)
+
+drawBall :: Ball -> Picture
+drawBall ball = Translate (ballx ball) (bally ball) (circleSolid ballRadius)
+
 initialWorld :: World
-initialWorld = World { player1 = (circleSolid 6.0),
-                       player2 = (circleSolid 6.0),
-                       ball = (circleSolid 5.0) }
+initialWorld = World { player1 = Player (-200) 0,
+                       player2 = Player 200 0,
+                       ball = Ball 0 0 }
 
 draw :: World -> Picture
-draw world = Translate 10 10 (ball world)
+draw world = Pictures [drawBall (ball world), drawPlayer (player1 world), drawPlayer (player2 world)]
               
 step :: Float -> World -> World
 step f world = world
@@ -27,7 +34,7 @@ event e world = world
 
 -- | Display the last event received as text.
 main
- = play (InWindow "GameEvent" (700, 600) (10, 10))
+ = play (InWindow "PONG" (500, 500) (10, 10))
         white
         1
         initialWorld
