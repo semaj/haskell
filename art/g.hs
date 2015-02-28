d6c275c3d28cfc42ff877ac15f93232a5abbd21d
James Larisch <root@jameslarisch.com>
2015-02-28 00:02:03 -0800

    pong pong haskell pong

--- art/g.hs
+++ art/g.hs
@@ -6,17 +6,17 @@ playerHeight = 100
 playerWidth = 10
 ballRadius = 9.0
 
-data Player = Player { playerx :: Float, playery :: Float } 
-data Ball = Ball { ballx :: Float, bally :: Float } 
+data Player = Player { px :: Float, py :: Float } 
+data Ball = Ball { bx :: Float, by :: Float } 
 data World = World { player1 :: Player,
                      player2 :: Player,
                      ball :: Ball }
 
 drawPlayer :: Player -> Picture
-drawPlayer player = Translate (playerx player) (playery player) (rectangleSolid playerWidth playerHeight)
+drawPlayer player = Translate (px player) (py player) (rectangleSolid playerWidth playerHeight)
 
 drawBall :: Ball -> Picture
-drawBall ball = Translate (ballx ball) (bally ball) (circleSolid ballRadius)
+drawBall ball = Translate (bx ball) (by ball) (circleSolid ballRadius)
 
 initialWorld :: World
 initialWorld = World { player1 = Player (-200) 0,
@@ -24,10 +24,37 @@ initialWorld = World { player1 = Player (-200) 0,
                        ball = Ball 0 0 }
 
 draw :: World -> Picture
-draw world = Pictures [drawBall (ball world), drawPlayer (player1 world), drawPlayer (player2 world)]
+draw world = Pictures [ drawBall (ball world),
+                        drawPlayer (player1 world),
+                        drawPlayer (player2 world) ]
+
+ballHittingPlayer :: Player -> Ball -> Bool
+ballHittingPlayer p b
+  | (bx b) /= (px p) = False
+  | bally < ptop && bally > pbot = True
+  | otherwise = False
+  where ptop = (py p) + (playerHeight / 2)
+        pbot = (py p) - (playerHeight / 2)
+        bally = by b
+
+ballHittingWall :: Ball -> Bool
+ballHittingWall b
+  | (by b) == 0 = True
+  | (by b) == 500 = True
+  | otherwise = False
+
+stepBall :: Player -> Player -> Ball -> Ball
+stepBall p1 p2 b
+  | ballHittingPlayer p1 b = Ball (- (bx b)) (by b)
+  | ballHittingPlayer p2 b = Ball (- (bx b)) (by b)
+  | ballHittingWall b = Ball (bx b) (- (by b))
               
 step :: Float -> World -> World
-step f world = world
+step f world = let p1 = player1 world
+                   p2 = player2 world
+                   b = ball world
+                   in World p1 p2 (stepBall p1 p2 (ball world))
+
 
 event :: Event -> World -> World
 event e world = world
